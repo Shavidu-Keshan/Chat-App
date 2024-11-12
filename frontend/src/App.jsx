b835dfc9cc709c26db5da2b5c0999ebd7e4fb818
Shavidu Keshan <shavidu4321@gmail.com>
2024-11-12 19:50:36 +0530

    create home page ui side bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import Login from './pages/login/Login.jsx'
 import SignUp from './pages/signup/SignUp.jsx'
 import './App.css'
+import Home from './pages/home/Home.jsx'
 
 function App() {
   
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <SignUp />
+      {/* <SignUp /> */}
+      <Home />
     </div>
   )
 }
