e8629419d705257eeda22225e8b7ddfb9116e01c
Shavidu Keshan <shavidu4321@gmail.com>
2024-11-15 16:12:12 +0530

    create use signup hook function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,20 @@ import Login from './pages/login/Login.jsx'
 import SignUp from './pages/signup/SignUp.jsx'
 import './App.css'
 import Home from './pages/home/Home.jsx'
+import { Route, Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <SignUp /> */}
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+      <Toaster/>
     </div>
   )
 }
